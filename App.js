734294562f22f2d2e82349a27d4023806ac0dc8a
mysilkyheart <deanheart09@gmail.com>
2019-01-08 08:25:23 +0700

    somechange

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import Home from './src/screens/Home'
-import { createStackNavigator, createAppContainer } from 'react-navigation'
+import Home from "./src/screens/Home";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-const App = createStackNavigator(
-    {
-        Home: Home
+const App = createStackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      header: null
     }
-)
+  }
+});
 
-export default createAppContainer(App)
+export default createAppContainer(App);
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,33 +1,35 @@
-import React, { Component } from 'react'
-import { Text, Button, View } from 'react-native'
-import axios from 'axios'
-import { connect } from 'react-redux'
-import { GET_USER } from '../redux/actions/user';
+import React, { Component } from "react";
+import { Text, Button, View } from "react-native";
+import axios from "axios";
+import { connect } from "react-redux";
+import { GET_USER } from "../redux/actions/user";
+import { TextInput } from "react-native-gesture-handler";
 
 class Home extends Component {
-
-  handleGetState = () => (
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then((response) => {
-      const { data } = response
-      this.props.dispatch(GET_USER(data))
-    })
-  )
-
+  handleGetState = () =>
+    axios.get("https://jsonplaceholder.typicode.com/todos/1").then(response => {
+      const { data } = response;
+      this.props.dispatch(GET_USER(data));
+    });
 
   render() {
     return (
-      <View>
-        <Text>{this.props.user.data.title}</Text>
-        <Button onPress={this.handleGetState} title='Button'>
-        </Button>
+      <View style={{ justifyContent: "center", flex: 1 }}>
+        <View style={{alignContent: 'center', margin: 20}}>
+          <Text style={{alignSelf:'center'}}> This is My Setting Framework </Text>
+          {/* <TextInput placeholder='Username'/>
+          <TextInput placeholder='Password'/>
+          <Button onPres          <TextInput placeholder='Username'/>
+          <TextInput placeholder='Password'/>
+          <Button onPress={this.handleGetState} title="Button" />s={this.handleGetState} title="Button" /> */}
+        </View>
       </View>
-
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   user: state.userReducers
-})
+});
 
-export default connect(mapStateToProps)(Home)
+export default connect(mapStateToProps)(Home);
